cb0957b7e698e85068d8f63d9e5f9d64a5c50fb3
Abderraouf MAOUDJ <raouf.maoudj@gmail.com>
2022-02-11 14:33:25 +0100

    Update:basic-cases-of-conflict

---
+++ swarmI4/renderer/pygame_graphics/info.py
@@ -0,0 +1,23 @@
+import csv
+import pandas
+class Info:
+    def __init__(self,text, is_title:bool=False):
+        """
+        class for storing info
+        """
+        self.is_title = is_title # True for title , False for normal text
+        self.text =text
+
+    @staticmethod
+    def store(data,file_path):
+        """
+        store the info in a csv file
+        """
+
+        dataframe = pandas.DataFrame(data)
+        print(dataframe)
+        with open(file_path, 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(data)
+
+
