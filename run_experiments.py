920a091c2c2d72c073e47462a4ecc4678e900e47
amaoudj <raouf.maoudj@gmail.com>
2023-05-08 15:47:10 +0200

    Update:ANTS2022

--- run_experiments.py
+++ run_experiments.py
@@ -441,7 +441,7 @@ def get_benchmark_data(bench_list):
         mapName = get_string_between_slashes(map_params[1][0])
 
 
-        if mapName == 'empty-48-48'or mapName == 'random-64-64-20' or mapName == 'warehouse-20-40-10-2-2':
+        if mapName == 'empty-48-48'or mapName == 'random-64-64-20' : #or mapName == 'warehouse-20-40-10-2-2':
             new_robot_set = []
             new_robot_set = [50,100,150, 200,250,300,350,400,450]#
 
